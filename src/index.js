8cda8b269dfb7174a23f605be312c971d196eacb
Noé Sánchez <noeszc@gmail.com>
2020-07-29 10:29:24 -0500

    create a  minimal apollo client

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import createClient from './lib/apollo'
+import { ApolloProvider } from '@apollo/client'
 
+// Create apollo client
+const client = createClient()
 const MOUNT_NODE = document.getElementById('app')
 
 const ConnectedApp = () => (
-  <div>ConnectedApp</div>
+  <ApolloProvider client={client}>
+
+  </ApolloProvider>
 )
 
 ReactDom.render(<ConnectedApp />, MOUNT_NODE)
---
+++ src/lib/apollo.js
@@ -0,0 +1,17 @@
+import {
+  InMemoryCache,
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+} from "@apollo/client";
+
+export default function createClient() {
+  const cache = new InMemoryCache();
+
+  const httpLink = new HttpLink({
+    credentials: "same-origin",
+    uri: process.env.REACT_APP_GRAPHQL_API_URL,
+  });
+
+  return new ApolloClient({ cache, link: ApolloLink.from([httpLink]) });
+}
